4ecb11f06f0b4c53c46fb3212fe47b4014ff5515
ElsaAsdar <elsaasdarr@gmail.com>
2024-05-01 13:50:29 +0800

    initial commit

--- elsaapps/models.py
+++ elsaapps/models.py
@@ -1,49 +1,107 @@
-# blog/models.py
-
 from django.db import models
-from django.utils import timezone
-from django.utils.text import slugify
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+import os
+from PIL import Image
 
-class Category(models.Model):
-    slug = models.SlugField(unique=True)
-    category = models.CharField(max_length=100)
- 
-    def __str__(self):
-        return self.category
+# Create your models here.
 
-class Page(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(upload_to='page_images/')
-    slug = models.SlugField(unique=True, max_length=255)
-    is_published = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+def get_topbanner_filename(instance, filename):
+    title = instance.title
+    slug = slugify(title)
+    return "documents/banner/{}/{}".format(slug, filename)
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(upload_to='post_images/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.TextField()
-    status = models.IntegerField(choices=[
-        (0, 'Draft'),
-        (1, 'Published'),
-    ], default=0)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200, null=True, blank=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    text = models.TextField()
+    date = models.DateField(auto_now_add=True, blank=True, null=True)
+    star = models.PositiveIntegerField(default=0)
+    topbanner =  models.ImageField(upload_to=get_topbanner_filename, null=True, blank=True)
+    category  = models.ForeignKey(Hashtag,  on_delete=models.CASCADE, null=True, blank=True)
+    
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('postdetail', args=[str(self.id)])
+
+#, default='documents/03.jpg'
+
+
+def get_image_filename(instance, filename):
+    title = instance.blog.id
+    slug = slugify(title)
+    return "post_images/post_images_{}/{}".format(slug, filename)
+
+
+class Document(models.Model):
+    blog = models.ForeignKey(Blog, default=None, on_delete=models.CASCADE)
+    image = models.FileField(upload_to=get_image_filename, verbose_name = 'Image')
+
+    def filename(self):
+        return os.path.basename(self.image.name)
+
+#-----------------------------------------------------------
+
+def get_user_image_filename(instance, filename):
+    title = instance.author
+    slug = slugify(title)
+    return "user_image_{}/{}".format(slug, filename)
+
+class UserImage(models.Model):
+    author = models.OneToOneField('auth.User', on_delete=models.CASCADE, null=True, default=True)
+    myimage = models.ImageField(upload_to=get_user_image_filename, default='user_image_noimg/jobs.jpg', null=True, blank=True)
+
+
+#------------------------------------------------------
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    message = models.CharField(max_length = 250)
+    usrimg = models.ForeignKey(UserImage, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.message
+
+
+class CommentReply(models.Model):
+    whichcomment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    message = models.CharField(max_length = 250)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    usrimg = models.ForeignKey(UserImage, on_delete=models.CASCADE, null=True, blank=True)
+
+    
+
+    def __str__(self):
+        return self.message
+
+
+
+
+
+#------------------------------------------------------
+
+class UserInfo(models.Model):
+    author = models.OneToOneField('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    displayname = models.CharField(max_length=50, null=True, blank=True)
+    designation = models.CharField(max_length=50, null=True, blank=True)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    views = models.PositiveIntegerField(default = 0)
+
+    def get_absolute_url(self):
+        return reverse('homepage')
+
+#-------------------------------------------------------
--- elsaapps/urls.py
+++ elsaapps/urls.py
@@ -1,13 +1,24 @@
-# blog/urls.py
-
-from django.urls import path
+from django.urls import path, include
+from blog import urls
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('categories/', views.category_list, name='category_list'),
-    path('posts/', views.post_list, name='post_list'),
-    path('pages/', views.page_list, name='page_list'),
-    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('page/<slug:slug>/', views.page_detail, name='page_detail'),
+    path('', views.Homepage.as_view(), name='homepage'),
+    path('aboutus', views.AboutUs.as_view(), name="aboutus"),
+    path('post/<int:pk>', views.PostDetail, name='postdetail'),
+    path('newpost', views.newpost, name='newpost'),
+    path('usrinfo', views.user_image_func, name='usrinfo'),
+    path('postedit/<int:pk>', views.PostEdit.as_view(), name='postedit'),
+    path('postdelete/<int:pk>', views.BlogDelete.as_view(), name='postdelete'),
+    path('mypost/<author>', views.MyPost.as_view(), name='myposts'),
+    path('commentreplydelete/<int:pk>', views.CommentReplyDelete.as_view(), name='commentreplydelete'),
+    path('commentdelete/<int:pk>', views.CommentDelete.as_view(), name='commentdelete'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('post/postnew', views.BlogPostNew.as_view(), name='postnew'),
+    path('profile/<str:nam>', views.profile, name='profile'),
+    path('editprofileinfo/<int:pk>', views.userInfoFormView, name='editprofileinfo'),
+    path('search', views.search, name='search'),
+    path('category/<str:hashtags>', views.categoryview, name='category'),
+
 ]
